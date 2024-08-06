b5c20d6b8dff5a68b1bbdb395190e6f0b89c4530
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-08-06 18:08:23 +0100

    combine data

--- investigate_exports.R
+++ investigate_exports.R
@@ -1,18 +1,23 @@
 folder <- "C:/Users/Anthony"
+cnames <- c("Title", "Year", "Abstract", "Keywords")
 
 #Scopus
 df1 <- read.csv(file.path(folder, "Downloads", "scopus.csv"))
 colnames(df1)
 df1[1,]
 df1$Abstract[1]
-df1[1,c("Title", "Year", "Abstract", "Index.Keywords")]
+df1_sub <- df1[, c("Title", "Year", "Abstract", "Index.Keywords")]
+colnames(df1_sub) <- cnames
 
 #Google Scholar (using Publish or Perish)
 df2 <- read.csv(file.path(folder, "Downloads", "PoPCites.csv"))
 colnames(df2)
 df2[1,]
 df2$Abstract[1]
-df2[1, c("Title", "Year", "Abstract")]
+df2_sub <- df2[, c("Title", "Year", "Abstract")]
+df2_sub <- cbind(df2_sub, data.frame("keywords"=""))
+colnames(df2_sub) <- cnames
+
 
 library(readxl)
 #Web of Science
@@ -20,4 +25,10 @@ df3 <- read_excel(file.path(folder, "Downloads", "savedrecs.xls"))
 colnames(df3)
 df3[1,]
 df3$Abstract[1]
-df3[1,c("Publication Year", "Article Title", "Abstract", "Author Keywords")]
+df3_sub <- df3[,c("Publication Year", "Article Title", "Abstract", "Author Keywords")]
+colnames(df3_sub) <- cnames
+
+
+df_all <- rbind(df1_sub, df2_sub, df3_sub)
+
+df_all
