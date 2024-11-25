624104fb452eaa7d33ff9e342f3f3043c73dde1f
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-11-25 17:06:17 +0000

    investigate merged bib

--- curl_bib.R
+++ curl_bib.R
@@ -27,14 +27,40 @@ for (i in 1:length(DOIlist)) {
     #try(curl_download(url, destfile = , handle = h, mode = "a"))
 }
 
-doi_df <- bib2df("curltest.bib")
+doi_df  <- bib2df("curltest.bib")
 doi_df2 <- bib2df("bib_extra.bib")
+nrow(doi_df2)
+doi_df2$YEAR %>% unique %>% sort()
 
 doi_df2 %>%
     nrow()
 
 doi_df3 <- bind_rows(doi_df, doi_df2)
+
+doi_df3$YEAR %>% unique %>% sort()
+
 View(doi_df3)
+
+doi_df4 <- doi_df3 %>% 
+    distinct(TITLE, .keep_all = TRUE) 
+    
+doi_df4 %>% 
+    nrow()
+
+doi_df4$CATEGORY %>% unique
+
+doi_df4 %>%
+    group_by(CATEGORY) %>%
+    summarise(n = n())
+    #filter(!(CATEGORY %in% c("PHDTHESIS", "PATENT", "BACHELORSTHESIS", "MASTERSTHESIS"))) %>%
+    #nrow()
+
+doi_df_final <- bib2df("merged_literature.bib")
+doi_df_final$MONTH %>% unique
+
+doi_df_final %>%
+    group_by(CATEGORY) %>%
+    summarise(n = n())
 
 missing_titles <- setdiff(titlelist, doi_df3$TITLE)
 missing_rows <- bib_df %>% filter(TITLE %in% missing_titles)
